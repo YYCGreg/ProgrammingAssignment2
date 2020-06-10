ed33d21577b795e2f78304c04305bf800fe51b1e
Greg Reed <65861372+YYCGreg@users.noreply.github.com>
2020-06-10 17:12:14 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creates a special "matrix" object to be cached with 
+## environment info for later use.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## 'x' is of class matrix() that will be converted into a 'special' matrix,
+        ## that will be passed to the cacheSolve() function to be inverted
+        
+        ## Returns an object that contains the special matrix with env. details
+        
+        s <- NULL ## initialize object for solved matrix
+        
+        ## Set the value of the matrix
+        set <- function(y) {
+                x <<- y ## Assign the input argument to x in the parent environment
+                s <<- NULL
+        } ## end of set function
+   
+        ## Get the value of the matrix 
+        get <- function() x
+        
+        ## Create solve function for environment to be called if cached
+        setSolve <- function(solve) s <<- solve
+        
+        ## Function to be called to get solved matrix
+        getSolve <- function() s
+        
+        ## Return special matrix with environment functions
+        list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
+        
+} ## end of function
 
-}
+## Function returns a solved matrix from either a previously cached
+## object or solves the passed matrix
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
+        
+        ## 'x' is an object of type makeCacheMatrix()
         ## Return a matrix that is the inverse of 'x'
-}
+
+        data <- x$getSolve()
+        ## if matrix is cached, then retrieve it
+        if(!is.null(data)) {
+                 message("Getting cached data")
+                 return(data) ## Return inverted matrix
+        } else {
+                data <- solve(x$get())
+                ## set the cached solve matrix
+                x$setSolve(data)
+                data ## Return inverted matrix
+                
+        } ## end of if
+
+} ## end of cacheSolve function
+
